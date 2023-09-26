8605e914dce41f4e7b5725c7236a6cf7cbd03ba5
schrutek <martin@schrutek>
2023-09-26 17:09:12 +0200

    Conflict successfuly merged

--- Datatypes/Spg.BasicsDemo/Source/Spg.BasicsDemo.App/Program.cs
+++ Datatypes/Spg.BasicsDemo/Source/Spg.BasicsDemo.App/Program.cs
@@ -7,6 +7,12 @@ Person p = new Person();
 
 class Person
 {
+    <<<<<<<
+    public int Id { get; set; }
+    =======
+    public int ID { get; set; }
+    >>>>>>>
+
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
 }
